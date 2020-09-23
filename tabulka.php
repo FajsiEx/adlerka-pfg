544d4e3c8e6c1aa9e0f9ab6f677a7023eeb79af5
FajsiEx <fajsiexofficial@gmail.com>
2020-09-23 08:18:24 +0200

    + php table

---
+++ tabulka.php
@@ -0,0 +1,27 @@
+<?php
+
+$dni = array("Pondelok", "Utorok", "Streda", "Štvrtok", "Piatok");
+$hodiny = array(0,1,2,3,4,5);
+
+?>
+
+<table border=1>
+    <tr>
+        <td>&nbsp;</td>
+        <? foreach ($hodiny as $hodina): ?>
+            <td><? echo $hodina; ?></td>
+        <? endforeach ?>
+    </tr>
+    <? foreach ($dni as $den): ?>
+    <tr>
+        <td><? echo $den; ?></td>
+        <? foreach ($hodiny as $hodina): ?>
+            <td> <!-- <? echo $hodina ?> --> </td>
+        <? endforeach; ?>
+    </tr>
+    <? endforeach; ?>
+</table>
+
+<br>
+
+<table></table>
