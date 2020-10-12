95fb363047f03b6dccd45b25c783734c6d336863
fajsiex <fajsiexofficial@gmail.com>
2020-10-12 12:53:23 +0200

    + responsivity

--- tabulka.php
+++ tabulka.php
@@ -12,8 +12,8 @@ if (!empty($_GET['jazyk'])) {
 
 var_dump($_SESSION['jazyk']);
 
-$dni = array("Pondelok", "Utorok", "Streda", "Štvrtok", "Piatok");
-$hodiny = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+$dni = array("Po", "Ut", "St", "Št", "Pi");
+$hodiny = array(0, 1, 2, 3, 4, 5, 6, 7);
 
 $servername = "localhost";
 $username = "root";
@@ -47,10 +47,14 @@ if (mysqli_num_rows($result) > 0) {
 
 //mysqli_close($conn);
 ?>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 
     <a href="tabulka.php?jazyk=sk">SLOVENSKY</a> | <a href="tabulka.php?jazyk=en">ANGLICKY</a>
 
-    <table border=1>
+    <table border=1 class="table">
         <tr>
             <td>Deň</td>
             <? foreach ($hodiny as $i => $hodina): ?>
